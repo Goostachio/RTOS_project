bba7afa5ca7320c34852d1000f916d0f07918ca6
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 02:11:46 +0700

    Update Light_control.cpp

--- Light_control.cpp
+++ Light_control.cpp
@@ -1,7 +1,10 @@
 #include "Light_control.h"
 #include <Arduino.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <DHT20.h>
 
-void setupLight (int LED){
+void setupLight (int LED) {
     switch (LED)
     {    
     case HEATER:
@@ -18,85 +21,101 @@ void setupLight (int LED){
         pinMode(D7, OUTPUT);
         pinMode(D8, OUTPUT);
         break;
+
+    case BOARD:
+        pinMode(48, OUTPUT);
+        break;
     }
 }
 
-void lightController (int LED, int color){
+void lightController (int LED, int color) {
     switch (LED)
     {
     case HEATER:
-        switch (color)
-        {
-        case RED:
-        digitalWrite(D3, HIGH);
-        digitalWrite(D4, HIGH);
+        switch (color) {
+
+          case RED:
+          digitalWrite(D3, HIGH);
+          digitalWrite(D4, HIGH);
           break;
         
-        case YELLOW:
-        digitalWrite(D3, LOW);
-        digitalWrite(D4, HIGH);
+          case YELLOW:
+          digitalWrite(D3, LOW);
+          digitalWrite(D4, HIGH);
           break;
 
-        case GREEN:
-        digitalWrite(D3, HIGH);
-        digitalWrite(D4, LOW);
+          case GREEN:
+          digitalWrite(D3, HIGH);
+          digitalWrite(D4, LOW);
           break;
 
-        case OFF:
-        digitalWrite(D3, LOW);
-        digitalWrite(D4, LOW);
+          case OFF:
+          digitalWrite(D3, LOW);
+          digitalWrite(D4, LOW);
           break;
         }
         break;
     
     case COOLER:
-        switch (color)
-        {
-        case RED:
-        digitalWrite(D5, HIGH);
-        digitalWrite(D6, HIGH);
+        switch (color) {
+
+          case RED:
+          digitalWrite(D5, HIGH);
+          digitalWrite(D6, HIGH);
           break;
         
-        case YELLOW:
-        digitalWrite(D5, LOW);
-        digitalWrite(D6, HIGH);
+          case YELLOW:
+          digitalWrite(D5, LOW);
+          digitalWrite(D6, HIGH);
           break;
 
-        case GREEN:
-        digitalWrite(D5, HIGH);
-        digitalWrite(D6, LOW);
+          case GREEN:
+          digitalWrite(D5, HIGH);
+          digitalWrite(D6, LOW);
           break;
 
-        case OFF:
-        digitalWrite(D5, LOW);
-        digitalWrite(D6, LOW);
+          case OFF:
+          digitalWrite(D5, LOW);
+          digitalWrite(D6, LOW);
           break;
         }
-        break;
+      break;
 
     case HUMID:
-        switch (color)
-        {
-        case RED:
-        digitalWrite(D7, HIGH);
-        digitalWrite(D8, HIGH);
+        switch (color) {
+
+          case RED:
+          digitalWrite(D7, HIGH);
+          digitalWrite(D8, HIGH);
           break;
         
-        case YELLOW:
-        digitalWrite(D7, LOW);
-        digitalWrite(D8, HIGH);
+          case YELLOW:
+          digitalWrite(D7, LOW);
+          digitalWrite(D8, HIGH);
           break;
 
-        case GREEN:
-        digitalWrite(D7, HIGH);
-        digitalWrite(D8, LOW);
+          case GREEN:
+          digitalWrite(D7, HIGH);
+          digitalWrite(D8, LOW);
           break;
         
-        case OFF:
-        digitalWrite(D7, LOW);
-        digitalWrite(D8, LOW);
+          case OFF:
+          digitalWrite(D7, LOW);
+          digitalWrite(D8, LOW);
           break;
         }
         break;
-    }
+
+    case BOARD:
+        switch(color){
+
+          case ON:
+          digitalWrite(48, HIGH);
+          break;
+
+          case OFF:
+          digitalWrite(48, LOW);
+          break;
+        }
+    }  
 }
