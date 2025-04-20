066914327b64c0b55c1eac7c228e46cdb042fbe8
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 00:41:10 +0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,11 +1,10 @@
 #include <scheduler.h>
 #include <software_timer.h>
-#include <HT_Sensor.h>
 #include <Cooler_task.h>
 #include <Heater_task.h>
 #include <Humidifier_task.h>
 #include <Blinky_task.h>
-
+#include <HT_Sensor.h>
 
 void TIMER_ISR(void *pvParameters) {
   while (1) {
@@ -15,23 +14,20 @@ void TIMER_ISR(void *pvParameters) {
 }
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+
   xTaskCreate(TIMER_ISR, "TIMER_ISR", 2048, NULL, 2, NULL);
 
-  pinMode(D3, OUTPUT);  pinMode(D4, OUTPUT);
-  pinMode(D5, OUTPUT);  pinMode(D6, OUTPUT);
-  pinMode(D7, OUTPUT);  pinMode(D8, OUTPUT);
-  pinMode(D9, OUTPUT);  pinMode(D10, OUTPUT);
-
-  pinMode(48, OUTPUT);
-
   SCH_Init();
-  SCH_Add_Task(timerRun, 0, 1);
-  SCH_Add_Task(humidifierRun, 0, 1);
-  SCH_Add_Task(coolerRun, 0, 1);
 
-  SCH_Add_Task(heater, 0, 100);
-  SCH_Add_Task(blinky, 0, 100);
-  SCH_Add_Task(htSensor, 0, 500);
+  SCH_Add_Task(timerRun, 5, 1);
+  SCH_Add_Task(humidifierRun, 3, 1);
+  SCH_Add_Task(coolerRun, 2, 1);
+
+  SCH_Add_Task(heater, 1, 100);
+  SCH_Add_Task(blinky, 4, 100);
+  SCH_Add_Task(htSensor_read, 0, 500);
 }
 
 void loop() {
