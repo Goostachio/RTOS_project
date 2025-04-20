df29260e0dd2b841b394b5fe0ba352a6a86665bc
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 00:41:33 +0700

    Update Light_control.h

--- Light_control.h
+++ Light_control.h
@@ -1,6 +1,15 @@
 #ifndef LIGHT_CONTROLLER_H_
 #define LIGHT_CONTROLLER_H_
 
+#define HUMID 1
+#define HEATER 2
+#define COOLER 3
 
+#define RED 1
+#define YELLOW 2
+#define GREEN 3
+
+void setupLight (int LED);
+void lightController (int LED, int color);
 
 #endif LIGHT_CONTROLLER_H_
