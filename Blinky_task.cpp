bef74b9c651f57951a50f5dc9fd5d822efaeab70
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 01:52:00 +0700

    Update Blinky_task.cpp

--- Blinky_task.cpp
+++ Blinky_task.cpp
@@ -1,9 +1,31 @@
 #include "Blinky_task.h"
 #include "software_timer.h"
-#include <Arduino.h>
+#include "Light_control.h"
 
-int status = 0;
+int blink = START;
 void blinky(){
-  status = 1 - status;
-  digitalWrite(LED_PIN, status);
+  switch (blink) {
+    case START:
+    setupLight(BOARD);
+    setTimer(4,100);
+    lightController(BOARD, ON);
+    blink = NO_BLINK;
+    break;
+
+    case BLINK:
+    lightController(BOARD, ON);
+    if(isTimerExpired(4) == 1){
+      setTimer(4,100);
+      blink = NO_BLINK;
+    }
+    break;
+
+    case NO_BLINK:
+    lightController(BOARD, OFF);
+    if(isTimerExpired(4) == 1){
+      setTimer(4,100);
+      blink = BLINK;
+    }
+    break;
+  }
 }
