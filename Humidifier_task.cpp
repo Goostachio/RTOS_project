f92c1a86e1f7a3bcd35232006c9719717884f250
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 00:43:13 +0700

    Update Humidifier_task.cpp

--- Humidifier_task.cpp
+++ Humidifier_task.cpp
@@ -1,1 +1,56 @@
 #include "Humidifier_task.h"
+#include "SensorData.h"
+#include "software_timer.h"
+#include "Light_control.h"
+
+float humidThreshold = 75;
+int humidState = INIT;
+
+int checkHumid (){
+    if (humidity > humidThreshold){
+        return GREEN;
+    }
+    else return CHECKING;
+}
+
+void humidifierRun(){
+    switch (humidState)
+    {
+    case INIT:
+    setupLight(HUMID);
+    humidState = checkHumid();
+    setTimer(3,500);
+    break;
+
+    case GREEN:
+    if (isTimerExpired(3) == 1){
+        humidState = YELLOW;
+        setTimer(3,300);
+        }
+    lightController(HUMID, GREEN);
+    break;
+
+    case YELLOW:
+    if (isTimerExpired(3) == 1){
+        humidState = RED;
+        setTimer(3,200);
+        }
+    lightController(HUMID, YELLOW);
+    break;
+
+    case RED:
+    if (isTimerExpired(3) == 1){
+        humidState = checkHumid();
+        setTimer(3,500);
+    }
+    lightController(HUMID, RED);
+    break;
+
+    case CHECKING:
+        if (isTimerExpired(3) == 1){
+            humidState = checkHumid();
+            setTimer(3,500);
+        }
+        break;
+    }
+}
