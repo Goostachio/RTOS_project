7a05bf25fd7c2ee3d19e8e6993180b58208f207e
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 01:54:06 +0700

    Create Cooler_task.cpp

---
+++ Cooler_task.cpp
@@ -0,0 +1,40 @@
+#include "Cooler_task.h"
+#include "software_timer.h"
+#include "SensorData.h"
+#include <Light_control.h>
+
+int coolerState = INIT;
+float coolerThreshold = 33;
+
+int checkCooler (){
+    if (temperature > coolerThreshold){
+        return COOLING;
+    }
+    else return NOT_COOLING;
+}
+
+void coolerRun(){
+    switch (coolerState) {
+        case INIT:
+        setupLight(COOLER);
+        coolerState = checkCooler();
+        setTimer(2,500);
+        break;
+
+        case COOLING:
+        if (isTimerExpired(2) == 1){
+            coolerState = checkCooler();
+            setTimer(2,500);
+        }
+        lightController(COOLER, GREEN);
+        break;
+
+        case NOT_COOLING:
+        if (isTimerExpired(2) == 1){
+            coolerState = checkCooler();
+            setTimer(2,500);
+        }
+        lightController(COOLER, RED);
+        break;
+    }
+}
