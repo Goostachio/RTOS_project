12f8f10a779aa1734c6dc9cb5076462159926d9f
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 02:11:27 +0700

    Update Light_control.h

--- Light_control.h
+++ Light_control.h
@@ -1,13 +1,17 @@
 #ifndef LIGHT_CONTROLLER_H_
 #define LIGHT_CONTROLLER_H_
 
-#define HUMID 1
-#define HEATER 2
-#define COOLER 3
+#define HUMID 0
+#define HEATER 1
+#define COOLER 2
+#define BOARD 3
+#define SENSOR 4
 
-#define RED 1
-#define YELLOW 2
-#define GREEN 3
+#define RED 0
+#define YELLOW 1
+#define GREEN 2
+
+#define ON 3
 #define OFF 4
 
 void setupLight (int LED);
