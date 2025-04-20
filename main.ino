40c7b9b03ef02af85eaf75550a24c938333252ce
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 01:51:32 +0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -15,18 +15,18 @@ void TIMER_ISR(void *pvParameters) {
 }
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
 
   xTaskCreate(TIMER_ISR, "TIMER_ISR", 2048, NULL, 2, NULL);
 
   SCH_Init();
 
-  SCH_Add_Task(timerRun, 5, 1);
-  SCH_Add_Task(humidifierRun, 3, 1);
-  SCH_Add_Task(coolerRun, 2, 1);
 
-  SCH_Add_Task(heater, 1, 100);
+  SCH_Add_Task(heater, 1, 1);
+  SCH_Add_Task(coolerRun, 2, 1);
+  SCH_Add_Task(humidifierRun, 3, 1);
+  SCH_Add_Task(timerRun, 5, 1);
+
   SCH_Add_Task(blinky, 4, 100);
   SCH_Add_Task(htSensor_read, 0, 500);
 }
