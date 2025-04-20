54269f0104ca6f182f62cf0d5f21a913f572c65d
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-21 00:42:36 +0700

    Update Humidifier_task.h

--- Humidifier_task.h
+++ Humidifier_task.h
@@ -1,6 +1,12 @@
 #ifndef HUMIDIFIER_H_
 #define HUMIDIFIER_H_
 
+#define INIT 0
+#define GREEN 1
+#define YELLOW 2
+#define RED 3
+#define CHECKING 4
+
     void humidifierRun();
 
 #endif HUMIDIFIER_H_
