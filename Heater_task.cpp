4c92fe02344658e423f86e3ff9fa3cb40a2e5678
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-05-05 23:59:19 +0700

    Update Heater_task.cpp

--- Heater_task.cpp
+++ Heater_task.cpp
@@ -3,48 +3,46 @@
 #include "SensorData.h"
 #include "Light_control.h"
 
-int heaterState = CHECKING;
-float safe = 31;
-float slightly_risky = 32;
-float risky = 33;
+int heaterState = INIT;
+float safe = 20;
+float slightly_risky = 30;
+float risky = 40;
 
 int checkTemp (){
     if (temperature > risky || temperature < safe) return RISKY;
     else if (temperature > slightly_risky) return SLIGHTLY_RISKY;
     else return NOT_RISKY;
-
-
 }
 
 void heater(){
     switch (heaterState) {
-        case CHECKING:
+        case INIT:
         setupLight(HEATER);
         heaterState = checkTemp();
-        setTimer(1,500);
+        setTimer(4,500);
         lightController(HEATER, OFF);
         break;
 
         case NOT_RISKY:
-        if (isTimerExpired(1) == 1){
+        if (isTimerExpired(4) == 1){
             heaterState = checkTemp();
-            setTimer(1,500);
+            setTimer(4,500);
         }
         lightController(HEATER, GREEN);
         break;
 
         case SLIGHTLY_RISKY:
-        if (isTimerExpired(1) == 1){
+        if (isTimerExpired(4) == 1){
             heaterState = checkTemp();
-            setTimer(1,500);
+            setTimer(4,500);
         }
         lightController(HEATER, YELLOW);
         break;
 
         case RISKY:
-        if (isTimerExpired(1) == 1){
+        if (isTimerExpired(4) == 1){
             heaterState = checkTemp();
-            setTimer(1,500);
+            setTimer(4,500);
         }
         lightController(HEATER, RED);
         break;
