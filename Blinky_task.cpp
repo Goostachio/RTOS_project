75bbb6b8caa22ebc8d5dc2ab0d8d4f19eec5eb2c
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-05-05 23:57:07 +0700

    Update Blinky_task.cpp

--- Blinky_task.cpp
+++ Blinky_task.cpp
@@ -7,22 +7,22 @@ void blinky(){
   switch (blink) {
     case START:
     setupLight(BOARD);
-    setTimer(4,100);
+    setTimer(1,100);
     blink = BLINK;
     break;
 
     case BLINK:
     lightController(BOARD, ON);
-    if(isTimerExpired(4) == 1){
-      setTimer(4,100);
+    if(isTimerExpired(1) == 1){
+      setTimer(1,100);
       blink = NO_BLINK;
     }
     break;
 
     case NO_BLINK:
     lightController(BOARD, OFF);
-    if(isTimerExpired(4) == 1){
-      setTimer(4,100);
+    if(isTimerExpired(1) == 1){
+      setTimer(1,100);
       blink = BLINK;
     }
     break;
